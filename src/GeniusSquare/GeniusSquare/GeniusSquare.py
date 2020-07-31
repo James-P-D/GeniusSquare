dfbfab98ed1504144c4bdb1c0585ea7958a2b621
James Dorrington <jdorrington@hotmail.com>
2020-07-31 13:37:06 +0100

    Labels fine

--- src/GeniusSquare/GeniusSquare/GeniusSquare.py
+++ src/GeniusSquare/GeniusSquare/GeniusSquare.py
@@ -11,10 +11,11 @@ import threading
 # pygame screen
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-top_label = Label(0, 0, TOP_LABEL_WIDTH, TOP_LABEL_HEIGHT, "ROLL DICE")
+roll_dice_label = Label(0, 0, ROLL_DICE_LABEL_WIDTH, ROLL_DICE_LABEL_HEIGHT, "ROLL DICE")
 dice_strip = np.ndarray(TOTAL_DICE, DiceCell)
 col_labels = np.ndarray(COLS, Label)
 row_labels = np.ndarray(ROWS, Label)
+solve_label = Label(0, SOLVE_LABEL_TOP, SOLVE_LABEL_WIDTH, SOLVE_LABEL_HEIGHT, "SOLVE")
 
 # Global flags
 processing = False
@@ -58,18 +59,18 @@ def initialise():
     global dice_strip
     global grid
         
-    dice_strip[0] = DiceCell(CELL_WIDTH * 1, TOP_LABEL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, DICE_0_VALUES)
-    dice_strip[1] = DiceCell(CELL_WIDTH * 2, TOP_LABEL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, DICE_1_VALUES)
-    dice_strip[2] = DiceCell(CELL_WIDTH * 3, TOP_LABEL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, DICE_2_VALUES)
-    dice_strip[3] = DiceCell(CELL_WIDTH * 4, TOP_LABEL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, DICE_3_VALUES)
-    dice_strip[4] = DiceCell(CELL_WIDTH * 5, TOP_LABEL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, DICE_4_VALUES)
-    dice_strip[5] = DiceCell(CELL_WIDTH * 6, TOP_LABEL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, DICE_5_VALUES)
-    dice_strip[6] = DiceCell(CELL_WIDTH * 6, TOP_LABEL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, DICE_6_VALUES)
+    dice_strip[0] = DiceCell(CELL_WIDTH * 1, DICE_STRIP_TOP, CELL_WIDTH, CELL_HEIGHT, DICE_0_VALUES)
+    dice_strip[1] = DiceCell(CELL_WIDTH * 2, DICE_STRIP_TOP, CELL_WIDTH, CELL_HEIGHT, DICE_1_VALUES)
+    dice_strip[2] = DiceCell(CELL_WIDTH * 3, DICE_STRIP_TOP, CELL_WIDTH, CELL_HEIGHT, DICE_2_VALUES)
+    dice_strip[3] = DiceCell(CELL_WIDTH * 4, DICE_STRIP_TOP, CELL_WIDTH, CELL_HEIGHT, DICE_3_VALUES)
+    dice_strip[4] = DiceCell(CELL_WIDTH * 5, DICE_STRIP_TOP, CELL_WIDTH, CELL_HEIGHT, DICE_4_VALUES)
+    dice_strip[5] = DiceCell(CELL_WIDTH * 6, DICE_STRIP_TOP, CELL_WIDTH, CELL_HEIGHT, DICE_5_VALUES)
+    dice_strip[6] = DiceCell(CELL_WIDTH * 6, DICE_STRIP_TOP, CELL_WIDTH, CELL_HEIGHT, DICE_6_VALUES)
 
     for i in range(COLS):
-        col_labels[i] = Label(CELL_WIDTH * (i + 1), TOP_LABEL_HEIGHT + DICE_STRIP_HEIGHT, CELL_WIDTH, CELL_HEIGHT, get_col_label(i))
+        col_labels[i] = Label(CELL_WIDTH * (i + 1), COL_LABEL_STRIP_TOP, CELL_WIDTH, CELL_HEIGHT, get_col_label(i))
     for i in range(ROWS):
-        row_labels[i] = Label(0, TOP_LABEL_HEIGHT + DICE_STRIP_HEIGHT + ((i + 1) * CELL_HEIGHT), CELL_WIDTH, CELL_HEIGHT, get_row_label(i))
+        row_labels[i] = Label(0, ROLL_DICE_LABEL_HEIGHT + DICE_STRIP_HEIGHT + ((i + 1) * CELL_HEIGHT), CELL_WIDTH, CELL_HEIGHT, get_row_label(i))
 
 
 
@@ -82,13 +83,14 @@ def initialise():
 ###############################################
 
 def draw_ui():
-    top_label.draw(screen)
+    roll_dice_label.draw(screen)
     for dice in dice_strip:
         dice.draw(screen)
     for col_label in col_labels:
         col_label.draw(screen)
     for row_label in row_labels:
         row_label.draw(screen);
+    solve_label.draw(screen)
 
 ###############################################
 # game_loop()
